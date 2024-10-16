7f9b1b29c0d666082302af98be0795aa85e6013f
Maria Jose Villarreal <villarrealmariaj@gmail.com>
2024-10-16 18:39:51 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App
+      {/* <img src='../netflix-background-1920x1080.png'></img> */}
+      
+    </div>
+  )
+}
+
+export default App
