555a57751fe16b07435a7a37bd8a529d1ba380f0
Maria Jose Villarreal <villarrealmariaj@gmail.com>
2024-10-16 21:08:07 -0300

    Initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import Home from './pages/Home/Home'
+
 const App = () => {
   return (
-    <div>App
+    <div>
       {/* <img src='../netflix-background-1920x1080.png'></img> */}
-      
+      <Home/>
     </div>
   )
 }
---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './TitleCards.css'
+
+const TitleCards = () => {
+  return (
+    <div className='titleCard'>TitleCards</div>
+  )
+}
+
+export default TitleCards
