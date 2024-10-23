18bd338729a8d52185273c6b50811aa24b462aba
Maria Jose Villarreal <villarrealmariaj@gmail.com>
2024-10-22 21:38:07 -0300

    Adding cards section with mock movies and images

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
 
-import './TitleCards.css'
+import "./TitleCards.css";
 
-const TitleCards = () => {
+import cardsData from "../../assets/Cards/CardsData";
+
+const TitleCards = ({title, category}) => {
+  const cardsRef = useRef();
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    cardsRef.current.addEventListener('wheel', handleWheel);
+  }, []);
+
   return (
-    <div className='titleCard'>TitleCards</div>
-  )
-}
+    <div className="titleCard">
+      <h2>{title ? title: "Popular on Netflix"}</h2>
+      <div className="cardList" ref={cardsRef}>
+        {cardsData.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt="Card Image" />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default TitleCards
+export default TitleCards;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,53 @@
-import React from 'react'
+import React from "react";
 
-import './Home.css'
+import "./Home.css";
 
-import Navbar from '../../components/Navbar/Navbar'
-import Footer from '../../components/Footer/Footer'
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+
+import hero_banner from "../../assets/Hero-banner.webp";
+import hero_title from "../../assets/Hero-title.webp";
+import play_icon from "../../assets/play-icon.svg";
+import info_icon from "../../assets/info-icon.svg";
+import TitleCards from "../../components/TitleCards/TitleCards";
 
 const Home = () => {
   return (
-    <div className='home'>
+    <div className="home">
       <Navbar />
-      <Footer/>
+      <div className="hero">
+        <img src={hero_banner} alt="" className="bannerImg" />
+        <div className="heroCaption">
+          <img src={hero_title} alt="" className="captionImg"></img>
+          <p>
+            Framed for murder, Lincoln Burrows awaits execution at Fox River
+            State Penitentiary - until his brilliant brother reveals an
+            elaborate plan to escape.
+          </p>
+          <div className="heroButtons">
+            <button className="button">
+              {" "}
+              <img src={play_icon} />
+              Play
+            </button>
+            <button className="button darkButton">
+              {" "}
+              <img src={info_icon} />
+              More Info
+            </button>
+          </div>
+          <TitleCards />
+        </div>
+      </div>
+      <div className="moreCards">
+        <TitleCards title={"Blockbuster Movies"} />
+        <TitleCards title={"Only on Netflix"} />
+        <TitleCards title={"Upcoming"} />
+        <TitleCards title={"Top Pics for You"} />
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
- 
+export default Home;
